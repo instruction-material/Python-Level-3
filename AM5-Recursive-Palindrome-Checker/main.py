4b308665c8e5f7ee4ac200d5117f75b07efe3a08
Jacob Anderson <jacobdanderson@gmail.com>
2026-05-06 10:19:51 -0400

    Update course material and remove Replit metadata

--- AM5-Recursive-Palindrome-Checker/main.py
+++ AM5-Recursive-Palindrome-Checker/main.py
@@ -1,9 +1,13 @@
 def isPalindrome(str):
-  if len(str) <= 1:
-    return True
-  if str[0] != str[-1]:
-    return False
-  return (isPalindrome(str[1:-1]))
+    if len(str) <= 1:
+        return True
+    if str[0] != str[-1]:
+        return False
+    return (isPalindrome(str[1:-1]))
+
 
 n = input("\n\nEnter the string you would like to check. \nWe will check if it is a palindrome. \n")
 print(isPalindrome(n))
+
+
+def isPalindrome(str): return True if len(str) <= 1 else False if str[0] != str[-1] else (isPalindrome(str[1:-1]))
